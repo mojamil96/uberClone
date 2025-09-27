0ccdb795df6a4a0efb4bee45d0c46c6fd2f18289
Jamil <mouhammed.al.jamil@edag.com>
2025-09-27 16:56:14 +0200

    fix issue with starting app after changes

--- my-expo-app/app/(root)/(tabs)/_layout.tsx
+++ my-expo-app/app/(root)/(tabs)/_layout.tsx
@@ -3,10 +3,10 @@ import { Stack } from 'expo-router';
 const Layout = () => {
   return (
       <Stack>
-        <Stack.Screen name='' options={{ headerShown: false }} />
-        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack.Screen name='chat' options={{ headerShown: false }} />
+        <Stack.Screen name='home' options={{ headerShown: false }} />
+        <Stack.Screen name='profile' options={{ headerShown: false }} />
+        <Stack.Screen name='rides' options={{ headerShown: false }} />
         <Stack.Screen name='modal' options={{ presentation: 'modal', title: 'Modal', headerShown: false }} />
       </Stack>
   );
